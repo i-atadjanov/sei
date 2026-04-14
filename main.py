9398075131dc3b1a02de1a3d15d1780ab1b1fd8c
Ibragim <ibragim.atadjanov@gmail.com>
2026-04-14 10:04:11 +0500

    flake8 bandit pip-audit urllib3=1.26.4 is added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import hashlib
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -11,3 +12,15 @@ class DiscountRequest(BaseModel):
 def calculate_discount(request: DiscountRequest):
     final_price = request.price - (request.price * request.discount_percent/100)
     return {"final_price": final_price}
+
+def bad_style():
+  x=1
+  y=2
+  return x
+
+@app.get("/login")
+def login():
+    # Bandit will catch this weak MD5 hashing algorithm
+    m = hashlib.md5()
+    m.update(b"super_secret_password")
+    return {"token": m.hexdigest()}
